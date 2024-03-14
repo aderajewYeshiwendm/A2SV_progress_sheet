dc33eb5f490adede42204867ef02121bd7521228
Aderajew Yeshiwendm <148349360+aderajeY@users.noreply.github.com>
2024-03-14 23:58:24 +0300

    Create apply_operations.py

---
+++ apply_operations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+        res = []
+        for i in nums:
+            if i != 0:
+                res.append(i)
+
+        for i in nums:
+            if i == 0:
+                res.append(i)
+        return res
+        
