81a7dd71f2e8723af5ab5fd73c24446ef69a715f
Aderajew Yeshiwendm <148349360+aderajeY@users.noreply.github.com>
2024-03-19 09:40:17 +0300

    Time: 474 ms (32.86%) | Memory: 29.8 MB (47.50%) - LeetSync

---
+++ leetcode/easy/219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dct = {}
+        for i in range(len(nums)):
+            if nums[i] in dct and abs(dct[nums[i]] - i) <= k:
+                return True
+            dct[nums[i]] = i
+        return False
+        
+
+
+                
+        
