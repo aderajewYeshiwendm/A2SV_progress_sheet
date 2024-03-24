e974b9553a98b248e828a73d402a871f6ea840f1
Aderajew Yeshiwendm <148349360+aderajeY@users.noreply.github.com>
2024-03-24 21:38:40 +0300

    Time: 1781 ms (22.97%) | Memory: 141.5 MB (12.36%) - LeetSync

--- leetcode/easy/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ leetcode/easy/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,56 +1,45 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-
-        if [source, destination] in edges:
-            return True
-        if source == destination:
-            return True
-
-        # Create an adjacency list from edges
         graph = defaultdict(list)
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
 
-        print(graph)
 
         
         # BFS
-        visited = set()
-        queue = deque([source])
+        # visited = set()
+        # queue = deque([source])
 
-        while queue:
-            vertex = queue.popleft()
+        # while queue:
+        #     vertex = queue.popleft()
 
-            if vertex == destination:
-                return True
+        #     if vertex == destination:
+        #         return True
             
-            visited.add(vertex)
+        #     visited.add(vertex)
 
-            for neighbour in graph[vertex]:
+        #     for neighbour in graph[vertex]:
 
-                if neighbour not in visited:
-                    visited.add(neighbour)
-                    queue.append(neighbour)
+        #         if neighbour not in visited:
+        #             visited.add(neighbour)
+        #             queue.append(neighbour)
 
 
         # DFS
-        # visited = set()
+        visited = set()
 
-        # def dfs(current, destination, visited):
+        def dfs(current, destination, visited):
 
-        #     if current == destination:
-        #         return True
+            if current == destination:
+                return True
             
-        #     visited.add(current)
-        #     # print(f"visited {visited}")
+            visited.add(current)
 
-        #     for neighbour in graph[current]:
-        #         # print(f"neighbour {neighbour} and graph {graph[current]}")
-        #         if neighbour not in visited:
-        #             if dfs(neighbour, destination, visited):
-        #                 visited.add(neighbour)
-        #                 return True
-        #     return False
+            for neighbour in graph[current]:
+                if neighbour not in visited:
+                    if dfs(neighbour, destination, visited):
+                        return True
+            return False
 
-        # return dfs(source, destination, visited)
+        return dfs(source, destination, visited)
