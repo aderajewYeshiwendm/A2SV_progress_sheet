0b0ab141dd6d28dffec516afb9798b42c60d47ed
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-04-12 18:30:17 +0300

    Time: 878 ms (61.00%) | Memory: 30.4 MB (32.99%) - LeetSync

--- leetcode/easy/1813-maximum-erasure-value/maximum-erasure-value.py
+++ leetcode/easy/1813-maximum-erasure-value/maximum-erasure-value.py
@@ -2,18 +2,18 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         left, right = 0, 0
-        seen = set()
+        visited = set()
         res = 0
         curr_sum = 0
         
         while right < len(nums):
-            if nums[right] not in seen:
-                seen.add(nums[right])
+            if nums[right] not in visited:
+                visited.add(nums[right])
                 curr_sum += nums[right]
                 right += 1
                 res = max(res, curr_sum)
             else:
-                seen.remove(nums[left])
+                visited.remove(nums[left])
                 curr_sum -= nums[left]
                 left += 1
                 
