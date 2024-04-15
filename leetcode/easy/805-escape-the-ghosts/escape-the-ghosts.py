cf7d225b436978662f5b9a52405ac83f65628e0a
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-04-15 12:59:00 +0300

    Delete leetcode/easy directory

--- leetcode/easy/805-escape-the-ghosts/escape-the-ghosts.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
-        lst = []
-        ans = abs(target[0]) + abs(target[1])
-        for i in ghosts:
-            lst.append(abs(target[0]-i[0]) + abs(target[1]- i[1]))
-        return ans < min(lst)
-
-        
