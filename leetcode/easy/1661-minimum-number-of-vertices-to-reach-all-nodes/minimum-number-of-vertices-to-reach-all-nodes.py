8c0700ffb6364c9c6703ee58e6baaf9096291a83
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-04-16 15:25:12 +0300

    Time: 955 ms (35.40%) | Memory: 55.3 MB (29.51%) - LeetSync

--- leetcode/easy/1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
+++ leetcode/easy/1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,18 +1,11 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        all_nodes = set(range(n))
-    
-    # Initialize a set to store nodes with incoming edges
-        with_incoming_edges = set()
-
-        # Iterate through the edges to find nodes with incoming edges
-        for _, to_node in edges:
-            with_incoming_edges.add(to_node)
-
-        # Find the difference between all nodes and nodes with incoming edges
-        # This gives us the nodes without incoming edges
-        result = all_nodes - with_incoming_edges
-
-        # Convert the result set to a list and return it
-        return list(result)
+        income = collections.defaultdict(list)
+        for src, dst in edges:
+            income[dst].append(src)
+        result = []
+        for i in range(n):
+            if not income[i]:
+                result.append(i)
+        return result
         
