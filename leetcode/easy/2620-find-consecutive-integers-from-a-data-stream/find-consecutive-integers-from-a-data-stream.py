e4555c3c11502ef099bbacb231abe7a45d158e43
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-04-24 19:25:10 +0300

    Time: 352 ms (86.75%) | Memory: 43.1 MB (44.58%) - LeetSync

---
+++ leetcode/easy/2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.py
@@ -0,0 +1,18 @@
+class DataStream:
+
+    def __init__(self, value: int, k: int):
+        self.k = k
+        self.val = value
+        self.t = k
+        
+
+    def consec(self, num: int) -> bool:
+        if self.t > 0:
+            self.t -= 1
+            
+        if num != self.val:
+            self.t = self.k
+        
+        if self.t:
+            return False
+        return True
