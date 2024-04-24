9812bb4497a116fbf8ff6318654974a2f6895a0f
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-04-24 18:29:24 +0300

    Time: 117 ms (97.68%) | Memory: 18.3 MB (26.03%) - LeetSync

--- leetcode/easy/2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ leetcode/easy/2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -2,7 +2,7 @@ class Solution:
     def removeStars(self, s: str) -> str:
         stack = []
         for char in s:
-            if stack and char == '*':
+            if char == '*':
                 stack.pop()
             else:
                 stack.append(char)
