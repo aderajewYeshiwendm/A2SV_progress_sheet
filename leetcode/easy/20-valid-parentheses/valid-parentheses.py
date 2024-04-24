933bee3ffba88da4d6b7faf50bc0c68bd6b66c5b
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-04-24 17:59:16 +0300

    Time: 11 ms (82.72%) | Memory: 11.8 MB (26.17%) - LeetSync

---
+++ leetcode/easy/20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        brackets = {")": "(", "}": "{", "]": "["}
+        stack = []
+
+        for c in s:
+            if c in brackets.values():
+                stack.append(c)
+            elif c in brackets:
+                if stack == [] or stack.pop() != brackets[c]:
+                    return False
+            else:
+                return False
+
+        if len(stack) > 0:
+            return False
+        return True
+                
+        
+        
