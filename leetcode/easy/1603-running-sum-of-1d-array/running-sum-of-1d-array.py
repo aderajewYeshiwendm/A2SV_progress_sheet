79298ad8b3522ef052e511b76d49fdbb754ac8b2
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-05-04 11:20:31 +0300

    Time: 57 ms (7.26%) | Memory: 16.8 MB (50.53%) - LeetSync

--- leetcode/easy/1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ leetcode/easy/1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        presum = [0]*len(nums)
-        presum[0] = nums[0]
-        for i in range(1,len(nums)):
-            presum[i] = presum[i-1]+nums[i]
-        return presum
+        ans = [0] * len(nums)
+        for i in range(len(nums)):
+            ans[i] = sum(nums[0:i+1])
+        return ans
