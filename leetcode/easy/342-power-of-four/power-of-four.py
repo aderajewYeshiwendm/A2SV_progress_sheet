a0eb6b5a260872d809706fb660d307ffad1126f6
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-05-15 18:34:59 +0300

    Time: 43 ms (12.55%) | Memory: 16.6 MB (11.66%) - LeetSync

--- leetcode/easy/342-power-of-four/power-of-four.py
+++ leetcode/easy/342-power-of-four/power-of-four.py
@@ -1,7 +1,9 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n <= 0:
+        if n<=0:
             return False
-        while n % 4 == 0:
-            n //= 4
-        return n == 1
+        if n==1:
+            return True
+        if n%4!=0:
+            return False
+        return self.isPowerOfFour(n//4)
